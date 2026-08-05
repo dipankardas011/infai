21492f66f72d75ef392de0923ef8c953cd7298bf
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2026-08-05 12:04:14 +0530

    some more todos
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- agent/harness-engine.go
+++ agent/harness-engine.go
@@ -13,3 +13,6 @@ func (*HarnessEngine) Start() {}
 func (*HarnessEngine) Stop()  {}
 
 // we need the infai to have a tool call with git which helps to get most out of it interms of lines its focusing to get the previous history given to it what and why it was changed like those.
+// also for compaction latest information is put forward, then middle sweep and and a whole summary like thing just like deepseek and others.
+// need to understand if the compaction is a process of harness in anyway helping the model or its just the model who decides the underlaying strategy.
+// and if we can use a seperate model which is crazy good at compaction like deepseek.
