181227dc63baf5ed7c51ce7aae01bcf354e240bf
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2026-08-05 11:56:44 +0530

    added some more todos
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- agent/harness-engine.go
+++ agent/harness-engine.go
@@ -11,3 +11,5 @@ type HarnessEngine struct {
 
 func (*HarnessEngine) Start() {}
 func (*HarnessEngine) Stop()  {}
+
+// we need the infai to have a tool call with git which helps to get most out of it interms of lines its focusing to get the previous history given to it what and why it was changed like those.
